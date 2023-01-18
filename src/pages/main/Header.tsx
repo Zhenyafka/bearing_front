166760792bf520e0a537e7c619f2dff6e8ed73c5
zhenyafka <zhenyafka163@gmail.com>
2023-01-18 21:55:05 +0400

    change header css

--- src/pages/main/Header.tsx
+++ src/pages/main/Header.tsx
@@ -4,18 +4,19 @@ import s from './Header.css'
 
 export const Header = () =>{
     return (
-       <div className={s.logo}>
-           <div className="Header-Block">
+       <div  className="headerBlock">
+           <div className="headerBlock1">
             <img src={logo} alt="Logo"/>
         </div>
 
-           <div className="Header-Block1">
+           <div className="headerBlock2">
                 <h1>Contacts</h1>
                 <p>+7 937-077-33-47</p>
                 <p>+7 (846) 379-84-00</p>
                 <p>379-84-01</p>
                 <p>379-84-02</p>
         </div>
+           <div className={s.logo}></div>
        </div>
     )
 }
