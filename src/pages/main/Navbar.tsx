4d78869fd323475037877f6e2bd563c860ddbb7c
zhenyafka <zhenyafka163@gmail.com>
2023-04-07 13:33:49 +0400

    Change navbar file

--- src/pages/main/Navbar.tsx
+++ src/pages/main/Navbar.tsx
@@ -2,16 +2,37 @@ import {Catalogs} from "./Catalogs.tsx";
 import {AboutUs} from "./AboutUs.tsx";
 import {Basket} from "./Basket.tsx";
 import { useState } from "react";
-
-export const Navbar = () => {
-    const [isOpen, setIsOpen] = useState<boolean>()
+import { Link } from "react-router-dom";
 
 
+// export const Navbar = () => {
+//     const [isOpen, setIsOpen] = useState<boolean>()
+//
+//
+//     return (
+//         <div>
+//             <Catalogs/>
+//             <AboutUs/>
+//             <Basket/>
+//         </div>
+//     )
+// }
+
+const Navbar = () => {
     return (
         <div>
-            <Catalogs/>
-            <AboutUs/>
-            <Basket/>
+            <li>
+                <Link to="/Catalogs">Catalogs</Link>
+            </li>
+            <li>
+                <Link to="/AboutUs">AboutUs</Link>
+            </li>
+            <li>
+                <Link to="/Basket">Basket</Link>
+            </li>
+
         </div>
-    )
-}
+    );
+};
+
+export default Navbar;
