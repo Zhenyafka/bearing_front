3fab2cc10ca215b680776ecaa4de09ca4454f64a
zhenyafka <zhenyafka163@gmail.com>
2023-04-07 13:33:59 +0400

    Change App file

--- src/pages/main/App.tsx
+++ src/pages/main/App.tsx
@@ -1,16 +1,32 @@
 import * as React from 'react';
 import {Header} from "./Header.tsx";
+import {Catalogs} from "./Catalogs.tsx";
+import {AboutUs} from "./AboutUs.tsx";
+import {Basket} from "./Basket.tsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {Navbar} from "./Navbar.tsx";
 import {Footer} from "./Footer.tsx";
 import {TableBearing} from "./TableBearing2.tsx";
 
-export const App = () => {
+export function App () {
     return (
         <div>
+        <div>
             <Header/>
-            <Navbar/>
-            <TableBearing/>
-            <Footer/>
         </div>
+    <div>
+        <Router>
+            <Navbar/>
+            <Routes>
+                <Route path='/Catalogs' exact component={Catalogs}/>
+                <Route path='/AboutUs' component={AboutUs}/>
+                <Route path='/Basket' component={Basket}/>
+            </Routes>
+        </Router>
+    </div>
+    <div>
+        <Footer/>
+    </div>
+</div>
     )
 }
