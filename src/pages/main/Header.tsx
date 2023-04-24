aa53447e5a7a560a7a14565739946c637b88650f
zhenyafka <zhenyafka163@gmail.com>
2023-04-24 18:59:18 +0400

    Edit file with header component

--- src/pages/main/Header.tsx
+++ src/pages/main/Header.tsx
@@ -7,7 +7,9 @@ export const Header = () => {
         <div>
             <div>
                 <img src={logo} alt="Logo" className="headerBlock"/>
-                <h1 className="headerBlock2">+7 937-077-33-47 +7 (846) 379-84-00</h1>
+                <h1 className="headerBlock2">+7 937-077-33-47 +7(846) 379-84-00 </h1>
+
+
             </div>
             {/*<div className="header">+7 937-077-33-47 +7 (846) 379-84-00</div>*/}
             <div className={s.headerBlock}></div>
